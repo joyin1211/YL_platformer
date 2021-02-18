10acaad12da0e62dc3c17484b0b21f20ee6baa4f
joyin1211 <User1@WIN-7JKQ31MUP7B>
2021-02-18 23:19:43 +0400

    everything alright now

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import pygame
-import structs
+from structs import Board
+import os
+
+size = width, height = 800, 800
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('imgs', name)
+    image = pygame.image.load(fullname).convert()
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+board = Board(3, 3)
+board.set_view(200, 200, 100)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.get_click(event.pos)
+    screen.fill((0, 0, 0))
+    board.render(screen)
+    pygame.display.flip()
--- structs.py
+++ structs.py
@@ -1,21 +1,4 @@
 import pygame
-import  os
-
-size = width, height = 800, 800
-screen = pygame.display.set_mode(size)
-
-
-def load_image(name, color_key=None):
-    fullname = os.path.join('imgs', name)
-    image = pygame.image.load(fullname).convert()
-
-    if color_key is not None:
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    else:
-        image = image.convert_alpha()
-    return image
 
 
 class Board:
@@ -64,18 +47,3 @@ class Board:
         cell = self.get_cell(mouse_pos)
         if 0 <= cell[0] < self.width and 0 <= cell[1] < self.height:
             self.on_click(cell)
-
-
-board = Board(3, 3)
-board.set_view(200, 200, 100)
-character_image = load_image("character.png")
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
-    screen.fill((0, 0, 0))
-    board.render(screen)
-    pygame.display.flip()
